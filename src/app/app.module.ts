46699193bcd0e2c5e540244413756b25f5ee1f7b
pvlaskovski <31986018+pvlaskovski@users.noreply.github.com>
2022-04-16 09:57:41 +0200

    add basic Login Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreModule } from './core/core.module';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
-        SharedModule,     
+        SharedModule,  
+        CoreModule   
     ],
     providers: [],
     bootstrap: [AppComponent]
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,30 +1,42 @@
+// Angular Materials
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';  
+import {MatInputModule} from '@angular/material/input';
+
+//Modiles
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+//Components
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-
-// Angular Materials
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatMenuModule} from '@angular/material/menu';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
-  declarations: [
-    HeaderComponent,
-    FooterComponent,
-  ],
-  imports: [
-    CommonModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
-    MatMenuModule,
-  ],
-  exports: [
-      HeaderComponent,
-      FooterComponent,
-  ]
+    declarations: [
+        HeaderComponent,
+        FooterComponent,
+        LoginComponent,
+    ],
+    imports: [
+        CommonModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatButtonModule,
+        MatMenuModule,
+        MatCardModule,
+        MatFormFieldModule,
+        MatInputModule,
+    ],
+    exports: [
+        HeaderComponent,
+        FooterComponent,
+        LoginComponent,
+    ]
 })
 export class SharedModule { }
