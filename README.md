6408634e52cbda58c971d103ef2b73367ddbd765
pvlaskovski <31986018+pvlaskovski@users.noreply.github.com>
2022-04-17 14:55:08 +0200

    Update Login Form with validations and toggle password visibility button

--- README.md
+++ README.md
@@ -25,3 +25,10 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
+#Features
+##Login form
+* Model driven form
+* Separate field validation (email/password) and form subbmition validation
+* Login button disabled if form is not valid
+* Toggle visibility icon for the password field
--- src/app/core/components/login/login.component.ts
+++ src/app/core/components/login/login.component.ts
@@ -1,16 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { AuthService } from 'src/app/core/services/auth.service';
 
+
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
-  constructor( public authService: AuthService) { }
+    @ViewChild('loginForm') loginForm!: NgForm;
 
-  ngOnInit(): void {
-  }
+    isSuccesfullLogin: boolean = true;
+    isPasswordVisible: boolean = false;
+
+    constructor(public authService: AuthService) { }
+
+    ngOnInit(): void {
+
+    }
+
+    togglePasswordVisibility(): void{
+        this.isPasswordVisible = !this.isPasswordVisible;
+    }
+
+    onSubmit(): void {
+        console.log(this.loginForm.value);
+        const { email, password } = this.loginForm.value;
+        this.authService.logIn(email, password);
+        this.isSuccesfullLogin = this.authService.isLoggedIn;
+    }
 
 }
