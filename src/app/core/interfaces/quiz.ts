a7fffa4ec0b801945df8485b21a203e14fb24f78
pvlaskovski <31986018+pvlaskovski@users.noreply.github.com>
2022-04-24 01:11:23 +0200

    Update Quiz service with get all functionality and updated Quiz interface

--- src/app/core/interfaces/quiz.ts
+++ src/app/core/interfaces/quiz.ts
@@ -1,6 +1,7 @@
 import { IQuestion } from "./question";
 
 export interface IQuiz{
+    id?: string,
     title: string,
     topic: string,
     questions: IQuestion[],
--- src/app/core/services/quiz.service.ts
+++ src/app/core/services/quiz.service.ts
@@ -1,23 +1,48 @@
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+
+
 
 //Firestore
 import * as auth from 'firebase/auth';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/compat/firestore';
+import { AngularFirestore, AngularFirestoreDocument, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 import { IQuiz } from '../interfaces/quiz';
+import { Observable } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
 })
 export class QuizService {
 
-    constructor(private db: AngularFirestore) { }
+    items: Observable<IQuiz[]> | undefined;
+
+
+    constructor(private db: AngularFirestore) {
+
+
+    }
 
     addQuiz(quizz: IQuiz): void {
         const quizzesRef = this.db.collection('quizzes');
         const newQuiz = quizz;
         quizzesRef.add({ ...newQuiz });
     }
+
+    getAllQuizzes(): Observable<IQuiz[]> {
+        return this.db.collection('quizzes').snapshotChanges().pipe(
+            map(changes =>
+                changes.map(c => {
+                    const data = c.payload.doc.data() as IQuiz;
+                    data.id = c.payload.doc.id;
+                    return data;
+                })
+            )
+        )
+    }
+
+
+
 }
 
  // async addRecipe() {
