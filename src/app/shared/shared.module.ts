e377726e236a35dbd3b49b3bf6dd3b017d3fedda
pvlaskovski <31986018+pvlaskovski@users.noreply.github.com>
2022-04-23 21:26:13 +0200

    Update Quiz Details

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CustomProgressBarComponent } from './components/custom-progress-bar/custom-progress-bar.component';
+import { QuizDetailsComponent } from './components/quiz-details/quiz-details.component';
+
+
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatButtonModule} from '@angular/material/button';
+import {MatRadioModule} from '@angular/material/radio';
+
+
 
 
 @NgModule({
   declarations: [
     CustomProgressBarComponent,
+    QuizDetailsComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatExpansionModule,
+    MatButtonModule,
+    MatRadioModule,
   ],
   exports:[
       CustomProgressBarComponent,      
+      QuizDetailsComponent,
   ]
 })
 export class SharedModule { }
