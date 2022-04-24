d3df672328f7047c38b2dcfb35592a1913945df0
pvlaskovski <31986018+pvlaskovski@users.noreply.github.com>
2022-04-24 14:48:11 +0200

    Update visibility on header navigation based on logged in state of user

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css']
 })
 
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+    constructor(private authService: AuthService) { }
 
-  ngOnInit(): void {
-  }
+    isLoggedIn: boolean = false;
+
+    ngOnInit(): void {
+        this.isLoggedIn = this.authService.isLoggedIn;
+    }
 
 }
