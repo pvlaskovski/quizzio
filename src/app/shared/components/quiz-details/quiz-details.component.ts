c0deb367c9a167768925d86ff87173083a65c694
pvlaskovski <31986018+pvlaskovski@users.noreply.github.com>
2022-04-27 09:24:26 +0200

    Add quiz result calculation

--- src/app/shared/components/quiz-details/quiz-details.component.ts
+++ src/app/shared/components/quiz-details/quiz-details.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { AbstractControl, FormArray, FormBuilder, FormControl } from '@angular/forms';
+import { IQuestion } from 'src/app/core/interfaces/question';
 import { IQuiz } from 'src/app/core/interfaces/quiz';
+
 
 @Component({
     selector: 'app-quiz-details',
@@ -8,9 +11,10 @@ import { IQuiz } from 'src/app/core/interfaces/quiz';
 })
 export class QuizDetailsComponent implements OnInit {
 
-    constructor() { }
+    constructor(private formBuilder: FormBuilder) { }
 
     @Input() quiz!: IQuiz;
+
 
     isInEditMode: boolean = false;
 
@@ -24,10 +28,51 @@ export class QuizDetailsComponent implements OnInit {
     deleteQuestion(index: number) {
         if (index > -1 && index < this.quiz.questions.length) {
             this.quiz.questions.splice(index, 1)
-        }else{
+        } else {
             console.log('Index out of bounds');
-            
+
         }
+    }
+
+
+
+    submitQuiz(): void {
+        console.log('form submitted');
+        // console.log(this.quiz);
+        let myForm = document.getElementById('myForm');
+        let formData = new FormData(myForm as any);
+
+        let correctAnswers = [];
+        let incorrectAnswers = [];
+        let givenAnswers = [];
+
+        for (let index = 0; index < this.quiz.questions.length; index++) {
+            let currentAnswer = formData.get(index.toString())
+            givenAnswers.push(currentAnswer);
+        }
+
+        for (let index = 0; index < this.quiz.questions.length; index++) {
+            let currentAnswer = formData.get(index.toString());
+            let questionCorrectAnswers = this.quiz.questions[index].correctAnswers;
+
+            if (questionCorrectAnswers.length === 1) {
+                const isCorrect = currentAnswer?.toString() === questionCorrectAnswers[0].toString();
+                if (isCorrect) {
+                    correctAnswers.push(index.toString());
+                } else {
+                    incorrectAnswers.push(index.toString());
+                }
+            } else {
+                // TODO: for multiple choice questions
+            }
+
+
+        }
+
+        console.log('GIVEN ANSERS ARE: => ' + givenAnswers);
+        console.log('CORRECRT ANSWERS GIVEN ARE: => ' + correctAnswers);
+        console.log('INCORRECT ANSWERS GIVEN ARE: - > ' + incorrectAnswers);
+
     }
 
 }
