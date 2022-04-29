9741bdf280af12888d7f2caa32c9bb2564ade26d
pvlaskovski <31986018+pvlaskovski@users.noreply.github.com>
2022-04-29 16:07:56 +0200

    Add like/dislike functionality for the quiz pods and add star to show fav. items. User service updated

--- src/app/core/components/home/home.component.ts
+++ src/app/core/components/home/home.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { QuizService } from '../../services/quiz.service';
 import { map } from 'rxjs/operators'
 import { IQuiz } from '../../interfaces/quiz';
+import { UserService } from '../../services/user.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { IUser } from '../../interfaces/iuser';
+import { firstValueFrom } from 'rxjs';
 
 
 @Component({
@@ -12,13 +16,53 @@ import { IQuiz } from '../../interfaces/quiz';
 export class HomeComponent implements OnInit {
 
     quizzes: IQuiz[] | undefined;
+    localUser: IUser | undefined;
+    currentUser: IUser | undefined;
+    currentUserFavQuizzes: string[] | undefined;
 
-    constructor(private quizService: QuizService) { }
+    constructor(private quizService: QuizService, private userService: UserService, private snackBar: MatSnackBar) { }
 
     ngOnInit(): void {
+        this.localUser = JSON.parse(localStorage.getItem("user") || "");
+
+        this.userService.getUserById(this.localUser?.uid || '').subscribe(u =>{
+            this.currentUser = u;
+           
+            this.currentUserFavQuizzes = u.favoriteQuizzes;
+        })
+
         this.quizService.getAllQuizzes().subscribe(data => {
             this.quizzes = data;
         });
+    }
+
+    addToFavorite(userId: string, quizId: string): void {
+        this.userService.addFavoriteQuiz(userId, quizId).then(res => {
+            if (res) {
+                this.snackBar.open('Quiz has been added to your favourites', '', { duration: 2000, horizontalPosition: 'center', verticalPosition: 'top' })
+            } else {
+                this.snackBar.open('Quiz is already in your favourites', '', { duration: 2000, horizontalPosition: 'center', verticalPosition: 'top' })
+            }
+        })
+    }
+
+    removeFromFavorite(userId: string, quizId: string): void{
+        
+        this.userService.removeFavoriteQuiz(userId, quizId).then(res => {
+            if (res) {
+                this.snackBar.open('Quiz has been removed your favourites', '', { duration: 2000, horizontalPosition: 'center', verticalPosition: 'top' })
+            } else {
+                this.snackBar.open('Quiz is not in your favourites', '', { duration: 2000, horizontalPosition: 'center', verticalPosition: 'top' })
+            }
+        })
+    }
+
+    copyLink(recipeId: string): void{
+        let link = window.location.href + "take-quiz/" + recipeId;
+        navigator.clipboard.writeText(link);
+        if(link.length>0){
+            this.snackBar.open('Link for the quiz copied', '', { duration: 2000, horizontalPosition: 'center', verticalPosition: 'top' })
+        }
     }
 }
 
--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
 import * as auth from 'firebase/auth';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore, AngularFirestoreDocument, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
-import { map, Observable } from 'rxjs';
+import { firstValueFrom, map, Observable, tap } from 'rxjs';
 import { IUser } from '../interfaces/iuser';
 
 @Injectable({
@@ -14,13 +14,52 @@ export class UserService {
 
     constructor(private db: AngularFirestore) { }
 
-    getUserById(userId: string):  Observable<IUser> {
+    getUserById(userId: string): Observable<IUser> {
         return this.db.doc(`users/${userId}`).snapshotChanges().pipe(
             map(changes => {
                 const data = changes.payload.data() as IUser;
                 return data;
             })
         )
+    }
+
+    addFavoriteQuiz(userId: string, quizId: string): Promise<boolean> {
+
+        let userDoc = this.db.doc(`users/${userId}`);
+
+        return firstValueFrom(this.getUserById(userId))
+            .then(user => {
+                let favQ = user.favoriteQuizzes;
+
+                if (!favQ?.includes(quizId)) {
+                    favQ?.push(quizId);
+                    userDoc.update({ favoriteQuizzes: favQ });
+                    return true;
+                } else {
+                    return false;
+                }
+            })
+    }
+
+
+    removeFavoriteQuiz(userId: string, quizId: string): Promise<boolean> {
+
+        let userDoc = this.db.doc(`users/${userId}`);
+
+        return firstValueFrom(this.getUserById(userId))
+            .then(user => {
+                let favQ = user.favoriteQuizzes;
+
+                if (favQ?.includes(quizId)) {
+                    favQ = favQ?.filter(id => {
+                        return id !== quizId
+                    })
+                    userDoc.update({ favoriteQuizzes: favQ });
+                    return true;
+                } else {
+                    return false;
+                }
+            })
     }
 
 }
